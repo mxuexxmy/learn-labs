ea59374306ec5c96b3892a22538fc1be673da7d7
mxuexxmy <1989718921@qq.com>
2022-10-16 11:56:05 +0800

    mapstruct init

---
+++ mapstruct/src/main/java/xyz/mxue/learn/mapstruct/MapstructApplication.java
@@ -0,0 +1,16 @@
+package xyz.mxue.learn.mapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:mxuexxmy@gmail.com">mxuexxmy</a>
+ */
+@SpringBootApplication
+public class MapstructApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstructApplication.class, args);
+    }
+
+}
